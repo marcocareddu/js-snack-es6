2fcff39e0b908c1b76434e0bf42922cdc7ab98ae
marcocareddu <saldumaccu@tuta.io>
2023-06-13 16:30:06 +0200

    JS Student - Created students array

--- js/studentsScript.js
+++ js/studentsScript.js
@@ -1,1 +1,42 @@
 console.log('JS OK')
+
+// PHASE 1
+// Crate vars and arrays
+const students = [
+    {
+        id: 213,
+        name: `Marco della Rovere`,
+        grade: 78
+    },
+    {
+        id: 110,
+        name: `Paola Cortellessa`,
+        grade: 96
+    },
+    {
+        id: 250,
+        name: `Andrea Mantegna`,
+        grade: 48
+    },
+    {
+        id: 145,
+        name: `Gaia Borromini`,
+        grade: 74
+    },
+    {
+        id: 196,
+        name: `Luigi Grimaldello`,
+        grade: 68
+    },
+    {
+        id: 102,
+        name: `Piero della Francesca`,
+        grade: 50
+    },
+    {
+        id: 120,
+        name: `Francesca da Polenta`,
+        grade: 84
+    }
+
+];
