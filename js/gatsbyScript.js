bb4a6031de00c23b2f742bddd67b3a2624a2c8da
marcocareddu <saldumaccu@tuta.io>
2023-06-13 16:10:04 +0200

    JS Gatsby - Display result in console

--- js/gatsbyScript.js
+++ js/gatsbyScript.js
@@ -4,11 +4,16 @@ console.log('JS OK')
 // Crate vars and arrays
 const tableName = 'Tavolo Vip';
 const vipsName = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
-const invited = [];
 
 
-// ? Functions********************************
+// Create complete array
+const invited = vipsName.map((vip, index) => {
+    return {
+        table: tableName,
+        name: vip,
+        reservation: index + 1
+    }
+});
 
-
-
-// ? ******************************************
+// Display result in console
+console.table(invited);
