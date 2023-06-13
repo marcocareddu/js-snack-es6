55a02ea3a2c54a8add1c915e9ac28f707b88c4e3
marcocareddu <saldumaccu@tuta.io>
2023-06-13 15:55:08 +0200

    JS Gatsby - Crated vars and arrays

--- js/gatsbyScript.js
+++ js/gatsbyScript.js
@@ -1,1 +1,14 @@
 console.log('JS OK')
+
+// PHASE 1
+// Crate vars and arrays
+const tableName = 'Tavolo Vip';
+const vipsName = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
+const invited = [];
+
+
+// ? Functions********************************
+
+
+
+// ? ******************************************
