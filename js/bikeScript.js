1221377b1546f79bce090373261cf5fbcca386ab
marcocareddu <saldumaccu@tuta.io>
2023-06-14 16:07:09 +0200

    JS Bikes - Added FOR which is lighter

--- js/bikeScript.js
+++ js/bikeScript.js
@@ -33,3 +33,18 @@ let lighterBike = [];
 let bikesWeight = 10000;
 let toInject = '';
 
+
+// Verify which is lighter
+for (let element of bikes) {
+
+    // Deconstructing
+    const { name, weight } = element;
+
+    // Verify if is lighter
+    if (weight < bikesWeight) {
+        bikesWeight = weight;
+        lighterBike = [];
+        lighterBike.push(element);
+        toInject = `<p>La bici più leggera è la ${name} e pesa ${weight}kg </p>`
+    }
+};
