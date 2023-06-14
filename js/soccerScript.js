666be7bd5cb215ecbe10001d1d50b35810556fbf
marcocareddu <saldumaccu@tuta.io>
2023-06-14 16:15:42 +0200

    JS Soccer - Craeted Arrays & vars

--- js/soccerScript.js
+++ js/soccerScript.js
@@ -1,1 +1,42 @@
 console.log('**************************** SOCCER SCRIPT ****************************');
+
+// * PHASE 1
+// Link to DOM elements
+const soccerElement = document.getElementById('soccer');
+
+// Create Array
+const team = [
+    {
+        teamName: 'Juventus',
+        scoreDone: 0,
+        sufferedFouls: 0
+    },
+    {
+        teamName: 'Inter',
+        scoreDone: 0,
+        sufferedFouls: 0
+    },
+    {
+        teamName: 'Milan',
+        scoreDone: 0,
+        sufferedFouls: 0
+    },
+    {
+        teamName: 'Cagliari',
+        scoreDone: 0,
+        sufferedFouls: 0
+    },
+    {
+        teamName: 'Roma',
+        scoreDone: 0,
+        sufferedFouls: 0
+    },
+    {
+        teamName: 'Napoli',
+        scoreDone: 0,
+        sufferedFouls: 0
+    }
+];
+
+// Crate var
+let stringToInject = '';
